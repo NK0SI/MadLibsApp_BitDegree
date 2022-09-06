aea02ebca1842ebeafed31806856cbae4e988d87
NK0SI <108928923+NK0SI@users.noreply.github.com>
2022-09-06 15:05:52 +0200

    Still Trying

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let TheTale = '${ProtagonistName} Porter had been raised in a devoutly religious household, and believed there was no problem in the world that could not be solved by prayer. ${ProtagonistName} never had any reason to doubt this - so, when her faith failed her and she could not find comfort when she needed it most, she was utterly lost. Her twin sister, Carla, was hit and killed by a drunk driver when they were 18. ${ProtagonistName} was shattered and her faith seemed to be gone, along with Carla. Their home had been quiet and serious even before Carla died, but after, all joy was snuffed out. She desperately grasped at the spiritual explorations that had previously comforted her. Praying harder. Praying more often. Reading scripture. Reading scriptural self-help. Talking to her parents. Talking to her pastor. Nothing worked. ${ProtagonistName} could not find her God in her heart. For a long time after the death of Carla, ${ProtagonistName} lived an endless, droning, mind-numbing void of work and prayer and eating and sleeping kind of life. It felt pointless. She thought she would never be happy again. Her pastor told her that she might find the joy God gives and comfort in a very unexpected place someday. What did that mean, exactly? She kept looking but all she felt was despair.';
+
+let UserInputs = [];
+
+const ProtagonistName = "Libby";
+
+
+
+
+/* 
+Libby Porter had been raised in a devoutly religious household, and believed there was no problem in the world that could not be solved by prayer. Libby never had any reason to doubt this; so, when her faith failed her and she could not find comfort when she needed it most, she was utterly lost.
+
+Her twin sister, Carla, was hit and killed by a drunk driver when they were 18. Libby’s world collapsed and her faith seemed to be gone, along with Carla. Their home had been quiet and serious even before Carla died, but after, all joy was snuffed out.
+
+She desperately grasped at the spiritual explorations that had previously comforted her. Praying harder. Praying more often. Reading scripture. Reading scriptural self-help. Talking to her parents. Talking to her pastor.
+
+Nothing worked. Libby couldn’t find her God in her heart. For a long time after Carla’s death, Libby’s life was an endless, droning, mind-numbing void of work and prayer and eating and sleeping. It felt pointless. She thought she would never be happy again.
+
+Her pastor told her that she might find God’s joy and comfort in a very unexpected place someday. What did that mean, exactly? She kept looking but all she felt was despair.
+*/
